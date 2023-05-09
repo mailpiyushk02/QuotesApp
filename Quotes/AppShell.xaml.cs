66c8d6c9b43aebd328acb8d98cfd0c4ed4991983
mailpiyushk02 <mailpiyushk02@gmail.com>
2023-05-09 14:47:30 +0530

    Add project files.

---
+++ Quotes/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Quotes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Quotes/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Quotes;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
